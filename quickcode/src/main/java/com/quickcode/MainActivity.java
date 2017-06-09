976d4ffc322fd280e4de8e9570b337633864d8d1
Liangcj <liangcj@softpoweredu.com>
2017-06-09 18:21:06 +0800

    导入zxing包

--- quickcode/src/main/java/com/quickcode/MainActivity.java
+++ quickcode/src/main/java/com/quickcode/MainActivity.java
@@ -1,13 +1,42 @@
 package com.quickcode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.xys.libzxing.zxing.activity.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.scan);
+        textView = (TextView) findViewById(R.id.tv);
+        setListener();
+    }
+
+    private void setListener() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(MainActivity.this, CaptureActivity.class),0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+            Bundle bundle = data.getExtras();
+            String result = bundle.getString("result");
+            textView.setText(result);
+        }
     }
 }
