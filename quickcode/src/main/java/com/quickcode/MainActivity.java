3427349d9eed4dab2b04d14156ca85679e79ab80
Liangcj <Liangcj@softpower.com>
2017-06-10 19:50:12 +0800

    修改配置

--- quickcode/src/main/java/com/quickcode/MainActivity.java
+++ quickcode/src/main/java/com/quickcode/MainActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
