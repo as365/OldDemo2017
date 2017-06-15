ff02b168bf20d078ad71b769fdb31bea26b6bc67
Liangcj <Liangcj@softpower.com>
2017-06-15 22:30:50 +0800

    学习写UI

--- uidemo/src/androidTest/java/com/uidemo/ExampleInstrumentedTest.java
+++ uidemo/src/androidTest/java/com/uidemo/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.demo;
+package com.uidemo;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -7,7 +7,6 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.*;
 
 /**
@@ -22,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.demo", appContext.getPackageName());
+        assertEquals("com.uidemo", appContext.getPackageName());
     }
 }
