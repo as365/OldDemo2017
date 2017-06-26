d9ebcf50cfc710f648af4be565b5fa4ee8856402
Liangcj <Liangcj@softpower.com>
2017-06-26 22:07:48 +0800

    头部底部View

--- canadapter/src/main/java/com/canadapter/MainActivity.java
+++ canadapter/src/main/java/com/canadapter/MainActivity.java
@@ -1,14 +1,15 @@
 package com.canadapter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import com.zhy.adapter.recyclerview.wrapper.HeaderAndFooterWrapper;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 
 public class MainActivity extends AppCompatActivity {
     private List<String> list = new ArrayList<>();
@@ -22,7 +23,15 @@ public class MainActivity extends AppCompatActivity {
         list=initData();//初始化数据
         myAdapter = new MyAdapter(this,R.layout.item,list);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(myAdapter);
+        HeaderAndFooterWrapper mHeaderAndFooterWrapper = new HeaderAndFooterWrapper(myAdapter);
+
+        TextView t1 = new TextView(this);
+        t1.setText("Header 1");
+        TextView t2 = new TextView(this);
+        t2.setText("Header 2");
+        mHeaderAndFooterWrapper.addHeaderView(t1);
+        mHeaderAndFooterWrapper.addFootView(t2);
+        recyclerView.setAdapter(mHeaderAndFooterWrapper);
 
     }
 
