14000c3bd2376fc370b2b124e49c068d077cdec8
Liangcj <liangcj@softpoweredu.com>
2017-07-05 15:43:25 +0800

    自定义文本

--- uidemo/src/main/java/com/activity/MainActivity.java
+++ uidemo/src/main/java/com/activity/MainActivity.java
@@ -46,8 +46,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        init();
+        setContentView(R.layout.agree_activity);
+       // init();
     }
 
     private void init() {
